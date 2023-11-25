ebc335546b507f6dd1779ba6b7cb410c342c6626
Nico Gallinal <nicoabie@gmail.com>
2023-11-25 00:11:41 -0300

    add missing postgres test for aliasify

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -5,21 +5,35 @@ import { astify, aliasify } from './parser';
 
 describe('parser', () => {
   describe('aliasify', () => {
-    it('should do nothing when selecting * from one table', () => {
+    it('mysql - should do nothing when selecting * from one table', () => {
       assert.equal(
         aliasify('mysql', astify('mysql', 'select * from users')),
         'SELECT * FROM `users`',
       );
     });
 
-    it('should not add aliases when selecting from a single table', () => {
+    it('postgres - should do nothing when selecting * from one table', () => {
+      assert.equal(
+        aliasify('postgres', astify('postgres', 'select * from users')),
+        'SELECT * FROM "users"',
+      );
+    });
+
+    it('mysql - should not add aliases when selecting from a single table', () => {
       assert.equal(
         aliasify('mysql', astify('mysql', 'select id, name from users')),
         'SELECT `id`, `name` FROM `users`',
       );
     });
 
-    it('should alias when there are no aliases present in query', () => {
+    it('postgres - should not add aliases when selecting from a single table', () => {
+      assert.equal(
+        aliasify('postgres', astify('postgres', 'select id, name from users')),
+        'SELECT "id", "name" FROM "users"',
+      );
+    });
+
+    it('mysql - should alias when there are no aliases present in query', () => {
       assert.equal(
         aliasify(
           'mysql',
@@ -29,6 +43,19 @@ describe('parser', () => {
           ),
         ),
         'SELECT `u`.`name` AS `u.name`, `p`.`title` AS `p.title`, `p`.`content` AS `p.content` FROM `users` AS `u` INNER JOIN `posts` AS `p` ON `u`.`id` = `p`.`user_id`',
+      );
+    });
+
+    it('postgres - should alias when there are no aliases present in query', () => {
+      assert.equal(
+        aliasify(
+          'postgres',
+          astify(
+            'postgres',
+            'select u.name, p.title, p.content from users u inner join posts p on u.id = p.user_id',
+          ),
+        ),
+        'SELECT "u"."name" AS "u.name", "p"."title" AS "p.title", "p"."content" AS "p.content" FROM "users" AS "u" INNER JOIN "posts" AS "p" ON "u"."id" = "p"."user_id"',
       );
     });
   });
