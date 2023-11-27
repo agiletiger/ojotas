77fddb210f3159b071e37101cf2d159709892cdc
Nico Gallinal <nicoabie@gmail.com>
2023-11-27 10:18:27 -0300

    refactor getSelectedColumnsFromAst to provide type info

--- src/getSelectedColumnsFromAst.test.ts
+++ src/getSelectedColumnsFromAst.test.ts
@@ -1,39 +1,79 @@
 import { describe, it } from '../test/test-utils';
 import assert from 'node:assert';
 
-import { getSelectedColumnsFromAst } from './getSelectedColumnsFromAst';
+import {
+  SchemaTypes,
+  getSelectedColumnsFromAst,
+} from './getSelectedColumnsFromAst';
 import { astify } from './parser';
 import { Dialect } from './orm';
 
 describe('getSelectedColumnsFromAst', () => {
+  const types: SchemaTypes = {
+    users: {
+      id: { tsType: 'number', nullable: false },
+      name: { tsType: 'string', nullable: false },
+    },
+    posts: {
+      content: { tsType: 'string', nullable: true },
+      id: { tsType: 'number', nullable: false },
+      title: { tsType: 'string', nullable: true },
+      user_id: { tsType: 'number', nullable: true },
+    },
+  };
+
   it.each([
-    { dialect: 'mysql' as Dialect },
-    { dialect: 'postgres' as Dialect },
+    {
+      dialect: 'mysql' as Dialect,
+    },
+    {
+      dialect: 'postgres' as Dialect,
+    },
   ])('$dialect - should work for a single table', ({ dialect }) => {
     assert.deepEqual(
       getSelectedColumnsFromAst(
+        types,
         astify(dialect, 'select u.id, u.name from users u'),
       ),
       {
-        users: ['id', 'name'],
+        users: [
+          {
+            column: 'id',
+            nullable: false,
+            type: 'number',
+          },
+          {
+            column: 'name',
+            nullable: false,
+            type: 'string',
+          },
+        ],
       },
     );
   });
 
   it.each([
-    { dialect: 'mysql' as Dialect },
-    { dialect: 'postgres' as Dialect },
+    {
+      dialect: 'mysql' as Dialect,
+    },
+    {
+      dialect: 'postgres' as Dialect,
+    },
   ])('$dialect - should work for two tables', ({ dialect }) => {
     assert.deepEqual(
       getSelectedColumnsFromAst(
+        types,
         astify(
           dialect,
           'select u.name, p.title, p.content from users u inner join posts p on u.id = p.user_id',
         ),
       ),
       {
-        users: ['name'],
-        posts: ['title', 'content'],
+        users: [{ column: 'name', nullable: false, type: 'string' }],
+        posts: [
+          { column: 'title', nullable: true, type: 'string' },
+          { column: 'content', nullable: true, type: 'string' },
+        ],
       },
     );
   });
@@ -41,13 +81,14 @@ describe('getSelectedColumnsFromAst', () => {
   it('postgres - should support returning in update statements', () => {
     assert.deepEqual(
       getSelectedColumnsFromAst(
+        types,
         astify(
           'postgres',
           'update users set name = "nico" where id = 1 returning id',
         ),
       ),
       {
-        users: ['id'],
+        users: [{ column: 'id', nullable: false, type: 'number' }],
       },
     );
   });
@@ -55,13 +96,14 @@ describe('getSelectedColumnsFromAst', () => {
   it('postgres - should support returning in insert statements', () => {
     assert.deepEqual(
       getSelectedColumnsFromAst(
+        types,
         astify(
           'postgres',
           'insert into users (name) values ("eze") returning id',
         ),
       ),
       {
-        users: ['id'],
+        users: [{ column: 'id', nullable: false, type: 'number' }],
       },
     );
   });
--- src/getSelectedColumnsFromAst.ts
+++ src/getSelectedColumnsFromAst.ts
@@ -1,31 +1,62 @@
 import { Column, ColumnRef, From } from 'node-sql-parser';
 import { AST } from './parser';
+import { MapSqlTypeToTsTypeFn, TsType } from './mapSqlTypeToTsType';
 
-export const getSelectedColumnsFromAst = (ast: AST) => {
+type TableName = string;
+type ColumnName = string;
+
+export type SchemaTypes = Record<
+  TableName,
+  Record<ColumnName, { tsType: TsType; nullable: boolean }>
+>;
+
+export const getSelectedColumnsFromAst = (
+  types: SchemaTypes,
+  ast: AST,
+): Record<
+  TableName,
+  Array<{
+    column: string;
+    type: ReturnType<MapSqlTypeToTsTypeFn>;
+    nullable: boolean;
+  }>
+> => {
   //https://github.com/taozhi8833998/node-sql-parser/issues/1638
   if (ast.type === 'select' && ast.columns !== '*') {
     const from = ast.from as Array<From>;
-    return (ast.columns as Column[]).reduce(
-      (acc, curr) => {
-        const columnRef = curr.expr as ColumnRef;
-        const tableRef = columnRef.table;
-        const tableName = from.find((f) => f.as === tableRef).table;
-        acc[tableName] ??= [];
-        acc[tableName].push(columnRef.column);
-        return acc;
-      },
-      {} as Record<string, string[]>,
-    );
+    return (ast.columns as Column[]).reduce((acc, curr) => {
+      const columnRef = curr.expr as ColumnRef;
+      const tableRef = columnRef.table;
+      const tableName = from.find((f) => f.as === tableRef).table;
+      const columnName = columnRef.column;
+      const type = Object.entries(types[tableName]).find(
+        ([cn]) => cn === columnName,
+      )[1];
+      acc[tableName] ??= [];
+      acc[tableName].push({
+        column: columnName,
+        type: type.tsType,
+        nullable: type.nullable,
+      });
+      return acc;
+    }, {});
   }
 
-  if (ast.type === 'update' && ast.returning) {
+  if ((ast.type === 'update' || ast.type === 'insert') && ast.returning) {
     const from = (ast.table[0] as From).table;
-    return { [from]: ast.returning.columns.map((c) => c.expr.column) };
-  }
-
-  if (ast.type === 'insert' && ast.returning) {
-    const from = (ast.table[0] as From).table;
-    return { [from]: ast.returning.columns.map((c) => c.expr.column) };
+    return {
+      [from]: ast.returning.columns.map((c) => {
+        const columnName = c.expr.column;
+        const type = Object.entries(types[from]).find(
+          ([cn]) => cn === columnName,
+        )[1];
+        return {
+          column: columnName,
+          type: type.tsType,
+          nullable: type.nullable,
+        };
+      }),
+    };
   }
 
   return {};
